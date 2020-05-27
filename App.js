4fe91370d1495a7b2279a49ad95f809bdeac8334
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-27 18:21:20 +0900

    check login

--- App.js
+++ App.js
@@ -1,28 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
-import { AppLoading, Font, Asset } from "expo";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-community/async-storage";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
+import { YellowBox } from "react-native";
+import styles from "./styles";
 
 export default function App() {
 	const [loaded, setLoaded] = useState(false);
+	const [client, setClient] = useState(null);
+	const [isLoggedIn, setIsLoggedIn] = useState(null);
+	YellowBox.ignoreWarnings(["Remote debugger"]);
 	const preLoad = async () => {
 		try {
 			await Font.loadAsync({
 				...Ionicons.font,
 			});
-			await Asset.loadAsync(require("./assets/logo.png"));
+			await Asset.loadAsync([require("./assets/logo.png")]);
+			const cache = new InMemoryCache();
+			await persistCache({
+				cache,
+				storage: AsyncStorage,
+			});
+			const client = new ApolloClient({
+				cache,
+				...apolloClientOptions,
+			});
+			const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+			if (isLoggedIn === null || isLoggedIn === false) {
+				setIsLoggedIn(false);
+			} else {
+				setIsLoggedIn(true);
+			}
 			setLoaded(true);
+			setClient(client);
 		} catch (e) {
 			console.log(e);
 		}
 	};
 	useEffect(() => {
-		preLoad;
+		preLoad();
 	}, []);
-	return loaded ? (
-		<View>
-			<Text>Open up App.js to start working on your app!</Text>
-		</View>
+	return loaded && client && isLoggedIn !== null ? (
+		<ApolloProvider client={client}>
+			<ThemeProvider theme={styles}>
+				<View>{isLoggedIn === true ? <Text>I'm in</Text> : <Text>I'm out</Text>}</View>
+			</ThemeProvider>
+		</ApolloProvider>
 	) : (
 		<AppLoading />
 	);
