287e4f6bfd02e478cecac30b8ed4c753c8d90018
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-31 16:16:11 +0900

    refresh

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
+import { ScrollView, RefreshControl } from "react-native";
 import Loader from "../../components/Loader";
 import { useQuery } from "react-apollo-hooks";
 import { FEED_QUERY } from "../../sharedQueries";
@@ -13,6 +14,24 @@ const View = styled.View`
 const Text = styled.Text``;
 
 export default () => {
-	const { data, loading } = useQuery(FEED_QUERY);
-	return <View>{loading ? <Loader /> : null}</View>;
+	const [refreshing, setRefreshing] = React.useState(false);
+	const { data, loading, refetch } = useQuery(FEED_QUERY);
+
+	const refresh = async () => {
+		try {
+			setRefreshing(true);
+			await refetch();
+		} catch (e) {
+			console.log(e);
+		} finally {
+			setRefreshing(false);
+		}
+	};
+	return (
+		<ScrollView
+			refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refresh} />}
+		>
+			{loading ? <Loader /> : null}
+		</ScrollView>
+	);
 };
