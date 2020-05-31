162d842e166e66106209af918c3811e623299a71
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-31 16:03:41 +0900

    ApolloClient add operation

--- App.js
+++ App.js
@@ -34,6 +34,14 @@ export default function App() {
 			});
 			const client = new ApolloClient({
 				cache,
+				request: async (operation) => {
+					const token = await AsyncStorage.getItem("jwt");
+					operation.setContext({
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					});
+				},
 				...apolloClientOptions,
 			});
 			const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
