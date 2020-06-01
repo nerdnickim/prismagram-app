d0f5b9c13be7089ebf7aa7aea3f8840e403b04f3
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-06-01 13:46:25 +0900

    Make a Feed

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { Image } from "react-native";
+import { Image, Platform } from "react-native";
+import Swiper from "react-native-swiper";
+import constans from "../constans";
+import { Ionicons, SimpleLineIcons } from "@expo/vector-icons";
 
-const Container = styled.View``;
+const Container = styled.View`
+	margin-bottom: 16px;
+`;
 const Header = styled.View`
 	padding: 10px;
 	flex-direction: row;
@@ -22,12 +27,46 @@ const Location = styled.Text`
 	font-size: 12px;
 `;
 
-const Post = ({ user, location, files }) => {
+const InfoContainer = styled.View`
+	margin: 10px 0 0 10px;
+`;
+
+const IconsContainer = styled.View`
+	flex-direction: row;
+	margin-bottom: 8px;
+`;
+
+const IconContainer = styled.View`
+	margin-right: 12px;
+`;
+
+const Caption = styled.Text`
+	margin-top: 3px;
+`;
+
+const CommentsCount = styled.Text`
+	margin-top: 3px;
+	opacity: 0.5;
+	font-size: 12px;
+`;
+
+const Post = ({
+	user,
+	location,
+	files = [],
+	likeCount,
+	caption,
+	comments = [],
+	isLiked,
+}) => {
 	return (
 		<Container>
 			<Header>
 				<Touchable>
-					<Image style={{ width: 40, height: 40 }} source={{ uri: user.avatar }} />
+					<Image
+						style={{ width: 40, height: 40, borderRadius: 20 }}
+						source={{ uri: user.avatar }}
+					/>
 				</Touchable>
 				<Touchable>
 					<HeaderUserContainer>
@@ -36,6 +75,43 @@ const Post = ({ user, location, files }) => {
 					</HeaderUserContainer>
 				</Touchable>
 			</Header>
+			<Swiper style={{ height: constans.height / 2.5 }}>
+				{files.map((file) => (
+					<Image
+						key={file.id}
+						style={{ width: constans.width, height: constans.height / 2.5 }}
+						source={{ uri: file.url }}
+					/>
+				))}
+			</Swiper>
+			<InfoContainer>
+				<IconsContainer>
+					<Touchable>
+						<IconContainer>
+							<Ionicons
+								size={24}
+								name={Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"}
+							/>
+						</IconContainer>
+					</Touchable>
+					<Touchable>
+						<IconContainer>
+							<SimpleLineIcons size={24} name="bubble" color="black" />
+						</IconContainer>
+					</Touchable>
+				</IconsContainer>
+				<Touchable>
+					<Bold>{likeCount === 1 ? "1 likes" : `${likeCount} likes`}</Bold>
+				</Touchable>
+				<Caption>
+					<Bold>{user.username}</Bold> {caption}
+				</Caption>
+				<Touchable>
+					<CommentsCount>
+						See all {comments.length === 0 ? null : comments.length} comments
+					</CommentsCount>
+				</Touchable>
+			</InfoContainer>
 		</Container>
 	);
 };
