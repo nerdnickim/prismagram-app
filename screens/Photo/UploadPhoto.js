e5ac8f25ebcb6d10a4837111119e8c8168944c31
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-06-05 19:46:23 +0900

    upload photo

--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -1,10 +1,23 @@
 import * as React from "react";
 import styled from "styled-components";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Image } from "react-native";
+import axios from "axios";
+import { gql } from "apollo-boost";
+import { Image, Alert } from "react-native";
 import constans from "../../constans";
 import useInput from "../../hooks/useInput";
 import styles from "../../styles";
+import { useMutation } from "react-apollo-hooks";
+import { FEED_QUERY } from "../../sharedQueries";
+
+const UPLOAD = gql`
+	mutation upload($caption: String!, $files: [String!]!, $location: String) {
+		upload(caption: $caption, files: $files, location: $location) {
+			id
+			location
+			caption
+		}
+	}
+`;
 
 const View = styled.View`
 	flex: 1;
@@ -41,15 +54,64 @@ const Button = styled.TouchableOpacity`
 	justify-content: center;
 `;
 
-export default ({ route }) => {
+export default ({ route, navigation }) => {
+	const [loading, setLoading] = React.useState(false);
+	const photoS = route.params.photo;
 	const captionInput = useInput("");
 	const locationInput = useInput("");
+
+	const [uploadMutation] = useMutation(UPLOAD, {
+		refetchQueries: () => [{ query: FEED_QUERY }],
+	});
+
+	const handleSubmit = async () => {
+		if (captionInput.value === "" || locationInput.value === "") {
+			Alert.alert("All fields are required ");
+		}
+		const formData = new FormData();
+		const name = photoS.filename;
+		const [, type] = name.split(".");
+		formData.append("file", {
+			name: name,
+			type: type.toLowerCase(),
+			uri: photoS.uri,
+		});
+		try {
+			setLoading(true);
+			const {
+				data: { location },
+			} = await axios.post("http://localhost:4000/api/upload", formData, {
+				headers: {
+					"content-type": "multipart/form-data",
+				},
+			});
+
+			const {
+				data: { upload },
+			} = await uploadMutation({
+				variables: {
+					caption: captionInput.value,
+					location: locationInput.value,
+					files: [location],
+				},
+			});
+			if (upload.id) {
+				navigation.navigate("bottom");
+			}
+		} catch (e) {
+			console.log(e);
+			Alert.alert("Cant upload, try later");
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<View>
 			<Container>
 				<Image
 					style={{ width: constans.width / 4, height: constans.height / 8 }}
-					source={{ uri: route.params.photo.uri }}
+					source={{ uri: photoS.uri }}
 				/>
 				<Form>
 					<STextInput
@@ -65,7 +127,7 @@ export default ({ route }) => {
 						multiline={true}
 					/>
 				</Form>
-				<Button onPress={() => null}>
+				<Button onPress={handleSubmit}>
 					<Text>Upload</Text>
 				</Button>
 			</Container>
