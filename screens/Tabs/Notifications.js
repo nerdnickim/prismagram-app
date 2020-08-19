ae32f374ca39c1ba9f15a3da845966e8fd230f2a
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-08-19 22:31:50 +0900

    mend

--- screens/Tabs/Notifications.js
+++ screens/Tabs/Notifications.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FlatList, TouchableOpacity } from "react-native";
+import { FlatList, TouchableOpacity, Image } from "react-native";
 import styled from "styled-components";
 import { useQuery } from "react-apollo-hooks";
 import { ME } from "../../sharedQueries";
@@ -9,11 +9,13 @@ const View = styled.View``;
 
 const ItemView = styled.View`
 	flex-direction: row;
+	align-items: center;
+	margin: 8px 10px;
 `;
 
 const Text = styled.Text`
 	align-items: center;
-	margin: 0 10px;
+	margin: 0 3px;
 `;
 
 export default ({ navigation }) => {
@@ -30,10 +32,22 @@ export default ({ navigation }) => {
 	};
 	const { data, loading, refetch } = useQuery(ME);
 
-	const Item = ({ username }) => {
+	const Item = ({ user }) => {
+		const username = user.username;
 		return (
 			<ItemView>
-				<TouchableOpacity onPress={() => navigation.navigate("UserDetail", { username })}>
+				<TouchableOpacity
+					onPress={() => navigation.navigate("UserDetail", { username })}
+					style={{
+						display: "flex",
+						alignItems: "center",
+						flexDirection: "row",
+					}}
+				>
+					<Image
+						style={{ width: 30, height: 30, borderRadius: 20 }}
+						source={{ uri: user.avatar }}
+					/>
 					<Text>{username}</Text>
 				</TouchableOpacity>
 				<View>
@@ -52,14 +66,12 @@ export default ({ navigation }) => {
 					<FlatList
 						refreshing={refreshing}
 						onRefresh={onRefresh}
-						data={[
-							{
-								username: data?.me?.posts.map((p) => p.likes.map((l) => l.user.username)),
-								id: data?.me?.posts?.map((p) => p.likes.map((l) => l.user.id)),
-							},
-						]}
-						renderItem={({ item }) => item.username[0].map((i) => <Item username={i} />)}
-						keyExtractor={(item) => item.id}
+						data={[data?.me?.posts.map((l) => l.likes.map((a) => a))]}
+						renderItem={({ item }) =>
+							item[0].map((i) =>
+								data.me.id === i.user.id ? null : <Item key={i.user.id} user={i.user} />
+							)
+						}
 					/>
 				)
 			)}
